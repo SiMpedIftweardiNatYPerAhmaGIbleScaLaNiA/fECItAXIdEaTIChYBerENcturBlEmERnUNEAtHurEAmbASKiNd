c0b9945cbc8d540fe491b8d21f9eabd436b83d99
SiMpedIftweardiNatYPerAhmaGIbleScaLaNiA <73495952+SiMpedIftweardiNatYPerAhmaGIbleScaLaNiA@users.noreply.github.com>
2020-10-26 19:04:38 +0000

    Add files via upload

---
+++ public.lua
@@ -0,0 +1,35 @@
+--[[
+
+   
+  ██████  █     █░ ▒█████   ██▀███  ▓█████▄    ▄▄▄█████▓▓█████   ██████ ▄▄▄█████▓
+▒██    ▒ ▓█░ █ ░█░▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌   ▓  ██▒ ▓▒▓█   ▀ ▒██    ▒ ▓  ██▒ ▓▒
+░ ▓██▄   ▒█░ █ ░█ ▒██░  ██▒▓██ ░▄█ ▒░██   █▌   ▒ ▓██░ ▒░▒███   ░ ▓██▄   ▒ ▓██░ ▒░
+  ▒   ██▒░█░ █ ░█ ▒██   ██░▒██▀▀█▄  ░▓█▄   ▌   ░ ▓██▓ ░ ▒▓█  ▄   ▒   ██▒░ ▓██▓ ░ 
+▒██████▒▒░░██▒██▓ ░ ████▓▒░░██▓ ▒██▒░▒████▓      ▒██▒ ░ ░▒████▒▒██████▒▒  ▒██▒ ░ 
+▒ ▒▓▒ ▒ ░░ ▓░▒ ▒  ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒▓  ▒      ▒ ░░   ░░ ▒░ ░▒ ▒▓▒ ▒ ░  ▒ ░░   
+░ ░▒  ░ ░  ▒ ░ ░    ░ ▒ ▒░   ░▒ ░ ▒░ ░ ▒  ▒        ░     ░ ░  ░░ ░▒  ░ ░    ░    
+░  ░  ░    ░   ░  ░ ░ ░ ▒    ░░   ░  ░ ░  ░      ░         ░   ░  ░  ░    ░      
+      ░      ░        ░ ░     ░        ░                   ░  ░      ░           
+                                     ░                                           
+
+Version: Public release
+
+Last Updated: 
+
+]]
+
+--[[-Configuration-]]--
+
+--WARNING: DO NOT HAVE MORE THAN ONE OF THESE ENABLED AT ONCE!
+
+getgenv().GeneralReach = true
+getgenv().AuraReach = false
+
+--Reach Visuals
+
+local Color = Color3.fromRGB(0, 255, 255)
+local Size = 90
+local LineThickness = 0.3
+local Transparency = 0.8
+local SurfaceTransparency = 0
+local SurfaceColor = Color3.fromRGB(0, 255 , 255)
